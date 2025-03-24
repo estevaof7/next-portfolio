412731b995da42fbb717a7960b51aefa3f05c55f
user_arpia137 <user_arpia137@tribanco.com.br>
2025-03-24 09:33:14 -0300

    style about page

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -2,29 +2,35 @@
 
 import Image from 'next/image';
 import { aboutData } from '@/data/about-data';
+import { Progress } from '@/components/ui/progress';
 
 export default function About() {
-  //PARA TECKSTACK ... PRIMENG TEM UM COMPONENTE QUE CHAMA KNOB ... É ELE QUE EU QUERO ... MAS PARECE QUE NÃO TEM NO SHADCN
+  //tech-stack
+  //https://ui.shadcn.com/charts#area-chart
+  //depois: mobile
 
   return (
     <section
-      className="min-h-screen bg-[url(/img/background/bg.svg)] bg-cover"
+      className="min-h-screen bg-[url(/img/background/bg.svg)] bg-cover flex"
       id="about"
     >
-      <div>
+      <div className="flex max-w-6xl my-80 mx-auto gap-40">
         <Image
           src={aboutData.profileImg.src}
           alt={aboutData.profileImg.alt}
-          width={150}
-          height={150}
+          width={400}
+          height={400}
+          className="rounded-full w-sm h-sm"
         />
-        <div>
-          <h1>{aboutData.name}</h1>
-          <h2>{aboutData.title}</h2>
-          <p>{aboutData.description}</p>
+        <div className="flex flex-col">
+          <h1 className="text-4xl font-bold">{aboutData.name}</h1>
+          <h2 className="text-2xl font-bold">{aboutData.title}</h2>
+          <p className="mt-8 mb-16 mx-0">{aboutData.description}</p>
           <div>
             <p>{aboutData.techStackTitle}</p>
-            <div></div>
+            <div>
+              <Progress value={33} />
+            </div>
           </div>
         </div>
       </div>
