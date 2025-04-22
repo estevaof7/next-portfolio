4811c816e5bff5d6746df55ddb347c0dbb3d9b53
Estêvão Ferreira <estevaofset@hotmail.com>
2025-04-22 07:22:25 -0300

    responsividade projetos

--- src/app/components/projects/project.tsx
+++ src/app/components/projects/project.tsx
@@ -1,8 +1,10 @@
 import { Button } from '@/components/ui/button';
+import { useIsMobile } from '@/hooks/use-mobile';
 import { ProjectCard } from '@/models/projects-model';
 import { TechData } from '@/models/tech-model';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRef } from 'react';
 
 enum Position {
   EVEN = 'even',
@@ -11,6 +13,11 @@ enum Position {
 
 export function Project({ index, data }: { index: number; data: ProjectCard }) {
   const position = index % 2 === 0 ? Position.EVEN : Position.ODD;
+  const isMobile = useIsMobile();
+
+  const divPai = useRef(null);
+  //divPai.current.clientWidth
+  //divPai.current.offsetWidth
 
   return (
     <div
@@ -18,43 +25,96 @@ export function Project({ index, data }: { index: number; data: ProjectCard }) {
                   md:flex-row md:justify-between
                   ${position === Position.EVEN ? '' : 'md:flex-row-reverse'}`}
     >
-      <div className="space-y-4 w-full md:w-1/2 lg:w-3/5">
-        <div>
-          <h1 className="text-2xl sm:text-3xl lg:text-4xl font-medium">
-            {data.title}
-          </h1>
-          <h2 className="text-gray-400 mt-1">
-            {(data.start ? data.start + ' - ' : '') + data.end}
-          </h2>
-        </div>
-        <div>{data.description()}</div>
-        <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4">
-          <div className="flex space-x-3 sm:space-x-4 overflow-x-auto pb-2 sm:pb-0">
-            {data.tech.map((item: TechData) => (
-              <TechCard key={item.name} card={item} />
-            ))}
+      {!isMobile ? (
+        <>
+          <div className="space-y-4 w-full md:w-1/2 lg:w-3/5">
+            <div>
+              <h1 className="text-3xl lg:text-4xl font-medium">{data.title}</h1>
+              <h2 className="text-gray-400 mt-1">
+                {(data.start ? data.start + ' - ' : '') + data.end}
+              </h2>
+            </div>
+            <div>{data.description()}</div>
+            <div
+              className="flex flex-row justify-between items-center gap-4"
+              ref={divPai}
+            >
+              <div className="flex space-x-4 overflow-x-auto">
+                {data.tech.map((item: TechData) => (
+                  <TechCard key={item.name} card={item} size="desktop" />
+                ))}
+              </div>
+              <div className="flex space-x-4">
+                {data.buttons?.map((button) => (
+                  <Button
+                    key={button.text}
+                    className="text-base"
+                    variant={'secondary'}
+                  >
+                    <Link href={button.link} target="_blank">
+                      {button.text}
+                    </Link>
+                    teste!
+                  </Button>
+                ))}
+              </div>
+            </div>
           </div>
-          <div className="flex space-x-3 sm:space-x-4">
-            {data.buttons?.map((button) => (
-              <Button key={button.text} className="text-sm sm:text-base">
-                <Link href={button.link} target="_blank">
-                  {button.text}
-                </Link>
-              </Button>
-            ))}
+          <div className="w-full md:w-1/2 lg:w-2/5">
+            <Gif gifPath={data.gifPath} alt={data.alt} size="desktop" />
           </div>
-        </div>
-      </div>
-      <div className="w-full md:w-1/2 lg:w-2/5 order-first md:order-none">
-        <Gif gifPath={data.gifPath} alt={data.alt} />
-      </div>
+        </>
+      ) : (
+        <>
+          <div>
+            <h1 className="text-2xl font-medium">{data.title}</h1>
+            <h2 className="text-gray-400 mt-1">
+              {(data.start ? data.start + ' - ' : '') + data.end}
+            </h2>
+          </div>
+          <div className="w-full">
+            <Gif gifPath={data.gifPath} alt={data.alt} size="mobile" />
+          </div>
+          <div className="space-y-4 w-full">
+            <div>{data.description()}</div>
+            <div className="flex flex-row justify-between items-center gap-4">
+              <div className="flex space-x-3 overflow-x-auto pb-2">
+                {data.tech.map((item: TechData) => (
+                  <TechCard key={item.name} card={item} size="mobile" />
+                ))}
+              </div>
+              <div className="flex space-x-3">
+                {data.buttons?.map((button) => (
+                  <Button
+                    key={button.text}
+                    className="text-sm"
+                    variant={'secondary'}
+                  >
+                    <Link href={button.link} target="_blank">
+                      {button.text}
+                    </Link>
+                  </Button>
+                ))}
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
-function Gif({ gifPath, alt }: { gifPath: string; alt: string }) {
+function Gif({
+  gifPath,
+  alt,
+  size
+}: {
+  gifPath: string;
+  alt: string;
+  size: 'mobile' | 'desktop';
+}) {
   return (
     <div
-      className={`w-full h-auto rounded-lg sm:rounded-2xl bg-gray-300 ${gifPath.includes('sesc') && 'border-2'}`}
+      className={`w-full h-auto ${size === 'desktop' ? 'rounded-2xl' : 'rounded-lg'} bg-gray-300 ${gifPath.includes('sesc') && 'border-2'}`}
     >
       <Image
         src={gifPath}
@@ -62,21 +122,33 @@ function Gif({ gifPath, alt }: { gifPath: string; alt: string }) {
         width={500}
         height={500}
         unoptimized
-        className="rounded-lg sm:rounded-xl w-full h-auto"
+        className={`${size === 'desktop' ? 'rounded-xl' : 'rounded-lg'} w-full h-auto`}
       />
     </div>
   );
 }
 
-function TechCard({ card }: { card: TechData }) {
+function TechCard({
+  card,
+  size
+}: {
+  card: TechData;
+  size: 'mobile' | 'desktop';
+}) {
   return (
-    <div className="rounded-full w-10 h-10 sm:w-12 sm:h-12 bg-secondary relative flex-shrink-0">
+    <div
+      className={`rounded-full ${size === 'desktop' ? 'w-12 h-12' : 'w-10 h-10'} bg-secondary relative flex-shrink-0`}
+    >
       <Image
         src={card.src}
         alt={card.name}
-        width={30 * (card.zoom || 1)}
-        height={30 * (card.zoom || 1)}
-        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 sm:w-[30px] sm:h-[30px]"
+        width={
+          size === 'desktop' ? 30 * (card.zoom || 1) : 24 * (card.zoom || 1)
+        }
+        height={
+          size === 'desktop' ? 30 * (card.zoom || 1) : 24 * (card.zoom || 1)
+        }
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
       />
     </div>
   );
