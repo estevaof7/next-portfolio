d4ad163ab25bc6e3828aac363165900da4d100d7
Estêvão Ferreira <estevaofset@hotmail.com>
2025-05-17 11:41:10 -0300

    começando a concertar o bug do iphone 7

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,10 +4,27 @@ import { footerData } from '@/data/footer-data';
 import { FooterItemModel } from '@/models/footer-model';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Footer() {
+  const footer = useRef(null);
+  const [textColor, setTextColor] = useState('');
+
+  useEffect(() => {
+    if (footer.current) {
+      // Obtém todos os estilos computados do elemento
+      const computed = window.getComputedStyle(footer.current);
+
+      // Agora você tem acesso a todas as propriedades CSS do elemento
+      setTextColor(computed.getPropertyValue('color'));
+
+      // console.log('Background color:', backgroundColor);
+      // console.log('textColor:', textColor);
+    }
+  }, []);
+
   return (
-    <footer id="contact" className="bg-black text-gray-300">
+    <footer id="contact" className="bg-white text-gray-300" ref={footer}>
       <div className="max-w-6xl mx-auto pt-10 md:pt-16 lg:pt-20 pb-8 md:pb-10 px-4 md:px-6 lg:px-8">
         <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-6 lg:gap-4">
           {footerData.map((item: FooterItemModel, index: number) => (
@@ -43,6 +60,7 @@ export default function Footer() {
           ))}
         </div>
       </div>
+      <div>COR DO TEXTO: {textColor}</div>
     </footer>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,16 @@ export default function Home() {
   //instalar husky para pre-commit e executar o comando npx prettier src/ --write
   //adicionar this.portfolio
   //meses project-data ... fazer enum
+  //no meu celular...
+  //    menu lateral
+  //    cor do progress
+  //    cor dos cards menores
+  //    cor do texto do rodapé
 
   //currículo: passar sessão projetos para cima ?
   //footer: campos input para enviar email
   //ver se separa em categorias o tech stack ...
   //mobile: projetos > div com as tecnologias e botões > ver alguma maneira de usar o useRef para que quando a soma da width dos cards e da width dos botões for maior ou igual à width da div pai, para que haja a quebra
-  //no meu celular... menu lateral não está rolando ... cor do progress não está rolando
   //melhorar visual... pegar referências
   //problema hidratação
   //elementos carregar e disparar animação a medida que for fazendo o scroll?
